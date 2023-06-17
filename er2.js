bebd25579598c8e047f06e2668440fc3a74ea0b6
nimeshlife <111240354+nimeshlife@users.noreply.github.com>
2023-06-17 23:27:02 +0545

    Update er2.js

--- er2.js
+++ er2.js
@@ -1,14 +1,13 @@
 class fetc{
-  fet(template,video){
-          fetch(template).then(response=>{if(response.ok){
-          return response.json();}
-            }).then(data=>{
-      return this.nim(data,video)
-       }).catch(er=>{console.log(er)})
-  }
-       
-  
-   nim(a,bc){
+
+fet(template,video){
+            fetch(template).then(response=>{if(response.ok){
+            return response.json();}
+              }).then(data=>{
+        return this.nim(data,video)
+         }).catch(er=>{console.log(er)})
+    }    
+     nim(a,bc){
          let v= document.querySelectorAll(".original__movies")
           for (let i = 0; i < a.length; i++) {
               let num= i;
@@ -20,74 +19,65 @@ class fetc{
                b.src=a[i]["download_url"];
               v[0].appendChild(e);
               e.addEventListener("click",()=>{
-              this.yal(bc,num)
-                
+                fetch(bc).then(data=>{
+                  if(data.ok){
+                    return data.json()
+                  }
+                }).then(data=>{
+                   this.mmnn(num,data)
+                  
+                })
                 
               });
       }
           };
 
-  // raju=(a,bc)=>{
-  //    this.nim(a,bc)
-  // }
-async yal(all,num){
-  try{
-                 
-                 const ant=  await fetch(all)
-                 const data= await  ant.json()
-                //  this.mmnn();
-                this.mmnn(num,data)
-               window.open("/anw.html")
-                }catch(err){
-                  console.log(err)
-                }
-}
 
   mmnn=(d,e)=>{
-   this.ral(d,e)
-   console.log(d)
-  }
-  async ral(d,e){
-      try{
-      const har= e[d]["download_url"]
+  const har= e[d]["download_url"]
       console.log(har)
-      const dat2=await fetch("https://api.github.com/repos/nimeshlife/webhost/contents/cod",{
-                headers:{
-                  'Authorization':'Bearer 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'
-                }
-              
-              })
-
-              const pdat2 =await dat2.json()
-
-              const ano= await fetch("https://api.github.com/repos/nimeshlife/webhost/contents/cod",{
-                method:"PUT",
-                headers:{
-                  'Authorization':'Bearer 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',
-                  'Content-Type':'application/json'
-                },
-                body:JSON.stringify({
-                  message:'update file',
-                  content:btoa(har),
-                  sha: pdat2.sha
-                })});
-                if(ano.ok){
-                  console.log("FILE UPDATED");
-                   
-                }else{
-                  console.log("error")
-                }
-
-
-    }catch(error){
-      console.log(error
-        )
-    }
-      
-
+      fetch("https://api.github.com/repos/nimeshlife/webhost/contents/cod",{
+        headers:{
+                          'Authorization':'Bearer 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'
+                        }
+      } 
+     ).then(datae=>{
+        if(datae.ok){
+          return datae.json()
+        }
+      })
+      .then(dato=>{
+        console.log(dato)
+        fetch("https://api.github.com/repos/nimeshlife/webhost/contents/cod",{
+          method:"PUT",
+          headers:{
+            'Authorization':'Bearer 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',
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify({
+            message:'update file',
+            content:btoa(har),
+            sha: dato.sha
+          })
+        })
+        .then(data=>{
+            if(data.ok){
+              console.log("FILE UPDATED");
+               window.open("/anw.html")
+                   window.close("/netflix.html")
+            }
+          }).catch(error=>{
+            console.log(error)
+          })
+         
             
              
+         
+      }).catch(error=>{
+        console.log(error)
+      })
   }
+
 }
 a= new fetc();
 a.fet("https://api.github.com/repos/nimeshlife/npt/contents","https://api.github.com/repos/nimeshlife/videos/contents");
